67b6092bf468ee58dfb19bae90be4e9fa000d483
DeesseFortuna <31386846+DeesseFortuna@users.noreply.github.com>
2017-08-27 07:21:40 -0700

    Changed .txt temp files to .list
    this might help with the odds of randomly deleting someone's text files in the same directory when they run this.

--- bandcamp-backup.sh
+++ bandcamp-backup.sh
@@ -5,38 +5,38 @@ ARTIST="$2"
 echo ""
 echo "Pre-execution cleanup"
 rm ./index.html*
-rm ./*.txt
+rm ./*.list
 
 echo ""
 echo "Fetching page..."
 wget $URL #get the page
 
-grep -o '"\/album\/.*">' index.html > dirlist.txt #grep out the album urls
+grep -o '"\/album\/.*">' index.html > dirlist.list #grep out the album urls
 rm ./index.html* #delete temp index page(s)
 
-sed -e 's/^"//' dirlist.txt > dirlist2.txt #trim preceding double quote
-rm ./dirlist.txt
+sed -e 's/^"//' dirlist.list > dirlist2.list #trim preceding double quote
+rm ./dirlist.list
 
-NUMBER=`cat dirlist2.txt | wc -l` #get the length of dirlist2 (# of albums)
+NUMBER=`cat dirlist2.list | wc -l` #get the length of dirlist2 (# of albums)
 
 echo ""
 echo "Found" $NUMBER "album directories:"
-sed -e 's/">$//' dirlist2.txt | sudo tee -a dirlist.txt #trim trailing "> and print to console
-rm ./dirlist2.txt 
+sed -e 's/">$//' dirlist2.list | sudo tee -a dirlist.list #trim trailing "> and print to console
+rm ./dirlist2.list 
 
-sed -e 's/^\/album\///' dirlist.txt > albumlist.txt #trim preceding /album/
-sed -e 's/-/ /g' albumlist.txt > albumlist2.txt #replace - with ' '
-rm ./albumlist.txt
+sed -e 's/^\/album\///' dirlist.list > albumlist.list #trim preceding /album/
+sed -e 's/-/ /g' albumlist.list > albumlist2.list #replace - with ' '
+rm ./albumlist.list
 
 while IFS= read -r line; do
 	Target=`echo $line | tr [A-Z] [a-z] | sed -e 's/^./\U&/g; s/ ./\U&/g'` #capitalizes the first letter of each word
-	echo $Target >> albumlist.txt
-done < albumlist2.txt
-rm albumlist2.txt
+	echo $Target >> albumlist.list
+done < albumlist2.list
+rm albumlist2.list
 
 echo ""
 echo "Generated these pretty directory names:"
-cat albumlist.txt
+cat albumlist.list
 
 echo ""
 echo "Making artist directory..."
@@ -51,7 +51,7 @@ echo "Making album directories..."
 while IFS= read -r line; do
 	mkdir "$line"
 	echo "Directory" $line "made."
-done < ./../albumlist.txt
+done < ./../albumlist.list
 
 echo ""
 echo "Starting downloads..."
@@ -59,7 +59,7 @@ echo "Starting downloads..."
 COUNT=1
 while IFS= read -r line; do
 	echo "Progress: Item #" $COUNT "/" $NUMBER ":"
-	ALBUM=`tail -n+$COUNT ./../albumlist.txt | head -n1`
+	ALBUM=`tail -n+$COUNT ./../albumlist.list | head -n1`
 	echo "Descending to ./" $ALBUM
 	cd "$ALBUM"
 	echo "Downloading" $ALBUM
@@ -68,7 +68,7 @@ while IFS= read -r line; do
 	echo ""
 	cd ..
 	let "COUNT++"
-done < ./../dirlist.txt
+done < ./../dirlist.list
 
 echo ""
 echo "Ascending..."
@@ -76,7 +76,7 @@ cd ..
 
 echo ""
 echo "Cleaning up temp files..."
-rm ./*.txt
+rm ./*.list
 
 
 echo ""
